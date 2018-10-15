ca0c963e25dc303dd68e6c110a3e5b45a51a0caa
HoussemGu <44164061+HoussemGu@users.noreply.github.com>
2018-10-15 18:25:02 +0200

    Add files via upload

---
+++ R/squareroot.R
@@ -0,0 +1,9 @@
+#' Square root a number
+#'
+#' Takes in any numeric value and square root it
+#' @param x A numeric value to be square rooted
+#' @return The square root of the input
+#' @export
+squareroot <- function(x){
+  return(x^(1/2))
+}
---
+++ R/xpower.R
@@ -0,0 +1,11 @@
+#' raise a number to power
+#'
+#' Takes x and raise it to the power y
+#' @param x A numeric value to be raised to power
+#' @param y numeric value, the power
+#' @return x power y
+#' @export
+xpower <- function(x, y){
+  return(x^y)
+}
+
